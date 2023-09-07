d70e2feb421df44faf4bdc7f14251b07c53d5586
Riko Logwirno <logwirno.teknus@gmail.com>
2023-09-07 22:39:33 +0700

    feat(migration): Create Books controller, model, migration, and API Route

---
+++ database/migrations/2023_09_07_121426_create_books_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('authors');
+            $table->year('published_year');
+            $table->string('tags');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
